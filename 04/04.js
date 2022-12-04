d07b8f7e63fca3f0a660966f225f03de6c89f9c4
Lars-Erik <lea@markedspartner.no>
2022-12-04 12:07:19 +0100

    4.2 solved

--- 04/04.js
+++ 04/04.js
@@ -7,6 +7,13 @@ function isFullOverlap(a, b) {
         && a.x2 <= b.x2;
 }
 
+function isOverlap(a, b) {
+    return (a.x1 >= b.x1
+        && a.x1 <= b.x2)
+        || (a.x2 >= b.x1
+        && a.x2 <= b.x2);
+}
+
 function createPair(line) {
     let pair = line.split(',')
         .map(segs => {
@@ -19,7 +26,8 @@ function createPair(line) {
     let obj = {
         a: pair[0],
         b: pair[1],
-        hasOverlap: isFullOverlap(pair[0], pair[1]) || isFullOverlap(pair[1], pair[0])
+        hasFullOverlap: isFullOverlap(pair[0], pair[1]) || isFullOverlap(pair[1], pair[0]),
+        hasOverlap: isOverlap(pair[0], pair[1]) || isOverlap(pair[1], pair[0])
     }
     return obj;
 }
@@ -29,8 +37,19 @@ export function totalFullOverlaps(input) {
     let pairs = lines.map(createPair);
     
     let pairsOfTotalOverlap = pairs.filter(p => {
-        return p.hasOverlap;
+        return p.hasFullOverlap;
     });
 
     return pairsOfTotalOverlap.length;
+}
+
+export function totalOverlaps(input) {
+    let lines = asLines(getData(import.meta, input));
+    let pairs = lines.map(createPair);
+    
+    let pairsOfOverlap = pairs.filter(p => {
+        return p.hasOverlap;
+    });
+
+    return pairsOfOverlap.length;
 }
--- 04/04.spec.js
+++ 04/04.spec.js
@@ -1,14 +1,23 @@
 import { expect } from "chai";
-import { totalFullOverlaps } from './04.js';
+import { totalFullOverlaps, totalOverlaps } from './04.js';
 
-describe("cleaning the beach", () => {
+describe.only("cleaning the beach", () => {
     [
     {input:'sample', expected: 2},
     {input:'debug', expected: 1},
     {input:'input', expected: 588}
     ].forEach(({input, expected}) =>
-    it.only(`finds total full overlaps in ${input}`, () => {
+    it(`finds total full overlaps in ${input}`, () => {
         let result = totalFullOverlaps(input);
+        expect(result).to.equal(expected);
+    }));
+
+    [
+    {input:'sample', expected: 4},
+    {input:'input', expected: 911}
+    ].forEach(({input, expected}) =>
+    it(`finds total overlaps in ${input}`, () => {
+        let result = totalOverlaps(input);
         expect(result).to.equal(expected);
     }));
 });
